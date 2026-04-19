d2d096ab966550f87481ab0b13ebfc308cc10bc8
bavlybbb12 <bavlybbb12@gmail.com>
2026-04-19 22:02:07 +0200

    solved easy problems on leetcode on recursion

---
+++ DSA/Recursion/Leetcode509. Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+
+    vector<long long> arr ;
+    long long arr[100] ;
+    long long fib(int n) {
+
+        if ( n <= 1) return n ;
+        
+        if ( arr[n] != 0 ) return arr[n] ;
+
+        arr[n] = fib(n-2) + fib(n-1) ;
+        return arr[n] ;
+        
+    }
+};
