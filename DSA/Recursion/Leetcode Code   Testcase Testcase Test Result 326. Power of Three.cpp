6d4814b863e6440d8b247c1696d45675da6b4c86
bavlybbb12 <bavlybbb12@gmail.com>
2026-04-20 12:29:20 +0200

    solved more easy on recursion on leetcode

---
+++ DSA/Recursion/Leetcode Code   Testcase Testcase Test Result 326. Power of Three.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if ( n <= 0 ) return false ;
+        if ( n == 1 ) return true ;
+
+        if ( n % 3 == 0 ) return isPowerOfThree(n/3) ;
+        if ( n % 3 != 0 ) return false ;
+
+        return false ;
+
+    }
+};
