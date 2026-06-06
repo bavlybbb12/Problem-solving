7a920fda1494f008b9939ab348df22a258c1f615
bavlybbb12 <bavlybbb12@gmail.com>
2026-06-06 21:23:34 +0300

    Solved on stacks & queues

---
+++ DSA/Stack, queues/LeetCode 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stack>
+
+using namespace std ;
+
+class MyQueue {
+
+private :
+
+    stack <int> st1 ;
+    stack <int> st2 ;
+
+public:
+    MyQueue() {
+        
+    }
+    
+    bool empty() {
+        
+        return st1.empty() && st2.empty() ;
+
+    }
+
+    void push(int x) {
+        
+        st1.push(x) ;
+
+    }
+    
+    int pop() {
+        
+        if (st2.empty() ) {
+
+            while ( !st1.empty() ) {
+
+                st2.push(st1.top()) ;
+                st1.pop() ;
+
+            }
+        }
+        int top = st2.top() ;
+        st2.pop() ;
+        return top ;
+
+    }
+    
+    int peek() {
+        
+        if (st2.empty() ) {
+
+            while ( !st1.empty() ) {
+
+                st2.push(st1.top()) ;
+                st1.pop() ;
+
+            }
+        }
+        return st2.top() ;
+
+    }
+    
+
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
