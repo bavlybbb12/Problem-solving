a5b813b7ccd37ce7a9df2d21731ddcdf12e000c6
bavlybbb12 <bavlybbb12@gmail.com>
2026-06-14 12:30:00 +0300

    Solved on sorting techniques

---
+++ DSA/Sorting/LeetCode 75.Sort Colors.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        if (nums.empty()) return;
+        int low = 0, mid = 0, high = nums.size()-1;
+
+        while ( mid <= high ){
+
+            if ( nums[mid] == 0) swap(nums[low++], nums[mid++]);
+            else if ( nums[mid] == 1) mid++;
+            else swap(nums[mid], nums[high--]);
+
+        }
+
+    }
+};
