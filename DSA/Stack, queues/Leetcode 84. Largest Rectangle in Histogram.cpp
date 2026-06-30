3d6389ca2f2de9eeef1858ac28d1b06d9873f54d
bavlybbb12 <bavlybbb12@gmail.com>
2026-06-30 13:03:50 +0300

    Solved on stacktopic from neetcode 150

---
+++ DSA/Stack, queues/Leetcode 84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int largest = 0;
+        int n = heights.size();
+        
+        for (int i = 0; i <= n; i++) {
+
+            int current = (i == n) ? 0 : heights[i];
+            
+            while (!st.empty() && current < heights[st.top()]) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width = st.empty() ? i : (i - st.top() - 1);
+                
+                largest = max(largest, height * width);
+            }
+            st.push(i);
+        }
+        
+        return largest;
+    }
+};
---
+++ DSA/Stack, queues/Letcode 853. Car Fleet.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        if (n == 0) return 0;
+
+        vector<pair<int, int>> cars(n);
+        for (int i = 0; i < n; i++) {
+            cars[i] = {position[i], speed[i]};
+        }
+
+        sort(cars.begin(), cars.end());
+
+        stack<double> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            double time = (double)(target - cars[i].first) / cars[i].second;
+            
+            if (!st.empty() && time <= st.top()) {
+                continue; 
+            }
+            
+            st.push(time);
+        }
+
+        return st.size();
+    }
+};
