9f1ca14c99216d7ea52d77133328f99bda442b99
bavlybbb12 <bavlybbb12@gmail.com>
2026-07-15 23:10:09 +0300

    Solved a problem on linked lists

---
+++ DSA/Linked list/Leetcode143-Reorder List.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+
+class Solution {
+
+    public:
+    void reorderList(ListNode* head) {
+        
+        if (!head || !head->next) return;
+
+        vector<ListNode*> nodes;
+        ListNode* curr = head;
+        
+        while (curr) {
+            nodes.push_back(curr);
+            curr = curr->next;
+        }
+
+        int left = 0;
+        int right = nodes.size() - 1;
+
+        while (left < right) {
+
+            nodes[left]->next = nodes[right];
+            left++;
+
+            if (left == right) break;
+
+            nodes[right]->next = nodes[left];
+            right--;
+        }
+
+        nodes[left]->next = nullptr;
+
+    }
+
+};
