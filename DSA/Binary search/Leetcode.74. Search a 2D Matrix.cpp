d1b666dc098516422e39df050ee44cb3a3532aa9
bavlybbb12 <bavlybbb12@gmail.com>
2026-07-16 14:43:25 +0300

    Solved on bianry search from neetcode 150

---
+++ DSA/Binary search/Leetcode.74. Search a 2D Matrix.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+class Solution {
+
+    public:
+    
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+
+        if (matrix.empty() || matrix[0].empty()) return false;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int left = 0;
+        int right = m * n - 1;
+        
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+            
+            int mid_value = matrix[row][col];
+            
+            if (mid_value == target) return true;  
+            
+            else if (mid_value < target) left = mid + 1;
+
+            else right = mid - 1;
+
+        }
+        
+        return false;
+
+    }
+
+};
