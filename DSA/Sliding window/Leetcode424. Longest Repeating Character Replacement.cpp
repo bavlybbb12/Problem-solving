b9b3ef56d9783300a24261c892a9384374c36804
bavlybbb12 <bavlybbb12@gmail.com>
2026-07-28 09:52:48 +0300

    Solved on sliding window from neetcode 150

---
+++ DSA/Sliding window/Leetcode424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+
+    public:
+    
+    int characterReplacement(string s, int k) {
+        
+        vector<int> counts(26, 0);
+        int left = 0;
+        int max_freq = 0;
+        int max_len = 0;
+
+        for (int right = 0; right < s.length(); ++right) {
+            
+            counts[s[right] - 'A']++;
+            max_freq = max(max_freq, counts[s[right] - 'A']);
+
+            if ((right - left + 1) - max_freq > k) {counts[s[left] - 'A']--; left++;}
+
+            max_len = max(max_len, right - left + 1);
+        
+        }
+
+        return max_len;
+    
+    }
+
+};
